72dbbf33dd67d9284e4e607d0ed0b64a5bd36321
Karan Chaudhri <krchaudh@ncsu.edu>
2017-10-18 18:47:39 -0400

    Completing Consumer

--- DIC/Consumer.py
+++ DIC/Consumer.py
@@ -1,13 +1,21 @@
 from confluent_kafka import Consumer, KafkaError
-import configFile
 from configFile import *
+import readURL
+from Mongodb import MongoDb
+from Producer import KafkaProducer
+
+mongo = MongoDb()
+producer = KafkaProducer()
 
 c = Consumer({'bootstrap.servers': BOOTSTRAP_SERVERS, 'group.id': GROUP_ID})
 c.subscribe([TOPIC])
 
 running = True
 while running:
-    msg = c.poll()
-    if not msg.error():
-        print('Received message: %s' % msg.value().decode('utf-8'))
+    url = c.poll()
+    if not url.error():
+        children = readURL.getLinks(url.value())
+        mongo.insert_into_graph(url.value(), children)
+        producer.check_and_insert_urls(children)
+
 c.close()
