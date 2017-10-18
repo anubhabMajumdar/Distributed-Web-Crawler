d5c45deb0b06ea39c7c929fbb33399fb24d30cce
Karan Chaudhri <krchaudh@ncsu.edu>
2017-10-18 19:28:42 -0400

    DB collection change

--- DIC/Consumer.py
+++ DIC/Consumer.py
@@ -12,13 +12,15 @@ c.subscribe([TOPIC])
 
 running = True
 while running:
+    print("Waiting for url to be fetched")
     url = c.poll()
     if not url.error():
         print("Fetched Url from Kafka - " + url.value())
         children = readURL.getLinks(url.value())
-        print("All children of " + url.value() + " - " + str(children))
+        #print("All children of " + url.value() + " - " + str(children))
         mongo.insert_into_graph(url.value(), children)
         mongo.add_url_status(url.value(), "processed")
+        print("Going to produce now...")
         producer.check_and_insert_urls(children)
 
 c.close()
