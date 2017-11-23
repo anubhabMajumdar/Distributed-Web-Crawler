af28d6de1de99992af0237739eb3d4a28a884943
Anubhab Majumdar <anubhabmajumdar93@gmail.com>
2017-11-22 23:09:20 -0500

    added config API file

---
+++ configAPI.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
+from flask.ext.jsonpify import jsonify
+from flask import render_template
+
+app = Flask(__name__)
+api = Api(app)
+
+@app.route("/", methods=['GET'])
+def hello():
+	d = {}
+	f = open('configFile.py', 'r')
+	for line in f.readlines():
+		if line.startswith('#') or '=' not in line:
+			continue
+		# print line
+		var, value = line.split('=')
+		d[var.strip()] = value.rstrip().strip()
+	return jsonify(d)
+
+if __name__ == '__main__':
+     app.run(port='5002')
